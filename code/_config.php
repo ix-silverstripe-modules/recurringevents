07ed22824946e21195d7d863035a4b9ab6fb23ce
guywatson <guy.watson@internetrix.com.au>
2015-06-04 10:25:24 +1000

    config fixes

--- code/_config.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-
-// if(GridFieldDetailForm_ItemRequest::has_extension("GridFieldBetterButtonsItemRequest")) {
-// 	//the better buttons publish method calls Versioned::publish() directly. The publish function calls forceChange(); which ruins our $this->isChanged() checks
-// 	GridFieldDetailForm_ItemRequest::remove_extension("GridFieldBetterButtonsItemRequest");
-// 	GridFieldDetailForm_ItemRequest::remove_extension("GridFieldBetterButtonsItemRequest");
-// }
-
